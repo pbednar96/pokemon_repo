d6fd048efacfaa50b9bec79195821c108a77f02b
Petr Bednar <petr.bednar@finshape.com>
2024-05-28 21:59:20 +0200

    Add - request to openai api

---
+++ app/pokemon_openai.py
@@ -0,0 +1,37 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+my_api_key = os.getenv('API_KEY')
+
+client = OpenAI(api_key=my_api_key)
+
+MODEL = "gpt-3.5-turbo-0125"
+TEMP = 0.6
+
+def get_response_openai(input_data):
+    completion = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": "Your main job is convert unstructured text to structured JSON."
+                           "Data will be related with Pokemons."
+            },
+            {
+                "role": "user",
+                "content": input_data
+            }
+        ],
+        temperature=TEMP,
+        top_p=1
+    )
+    return completion.choices[0].message.function_call.arguments
+
+# print(get_response_openai("Bulbasaur is a small, squat Pokémon with blue-green skin and a big red bulb on its back. "
+#                           "This bulb is actually a seed that will eventually blossom into a flower. Bulbasaur is a "
+#                           "dual-type Grass and Poison Pokémon, and it's one of the first Pokémon you can choose as "
+#                           "a starter in the classic Pokémon Red and Blue games.  It evolves into Ivysaur at level 16,"
+#                           " and then Venusaur at level 32. Skill Jump and Overkill"))
